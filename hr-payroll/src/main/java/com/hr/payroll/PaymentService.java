33600129a16145af497e9d5d60ddf4136f663df9
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-04-21 15:02:25 -0300

    RestTemplate config

--- hr-payroll/src/main/java/com/hr/payroll/PaymentService.java
+++ hr-payroll/src/main/java/com/hr/payroll/PaymentService.java
@@ -1,16 +1,31 @@
 package com.hr.payroll;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.hr.payroll.model.Payment;
+import com.hr.payroll.model.dto.WorkerDTO;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(Long workerId, Integer days) {
-		Payment payment = new Payment("Gabriel", BigDecimal.TEN, days);
+		Map<String, Object> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId);
+
+		WorkerDTO worker = restTemplate.getForObject(String.format("%s/workers/{id}", workerHost), WorkerDTO.class, uriVariables);
+		Payment payment = new Payment(worker.getName(), worker.getDailyIncome(), days);
 		return payment;
 	}
 
