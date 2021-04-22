ce77e0ae5ebbfa680f28ddfe5ac33d6eaf3bbf44
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-04-22 00:11:24 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/hr/payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/hr/payroll/service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.hr.payroll.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hr.payroll.feignclient.WorkerFeignClient;
 import com.hr.payroll.model.Payment;
 import com.hr.payroll.model.dto.WorkerDTO;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class PaymentService {
@@ -13,9 +16,15 @@ public class PaymentService {
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	public Payment getPayment(Long workerId, Integer days) {
 		WorkerDTO worker = workerFeignClient.getWorker(workerId).getBody();
 		Payment payment = new Payment(worker.getName(), worker.getDailyIncome(), days);
+		return payment;
+	}
+
+	public Payment getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", BigDecimal.ZERO, days);
 		return payment;
 	}
 
