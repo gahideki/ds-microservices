6ad81a31cc4b539fe0dd618d854ab6fde7df5b64
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-04 19:53:33 -0300

    Db config server

--- hr-oauth/src/main/java/com/hr/oauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/hr/oauth/config/AuthorizationServerConfig.java
@@ -17,8 +17,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Value("${oauth.client.name}")
-    private String clientName;
+    @Value("${oauth.client.id}")
+    private String clientId;
 
     @Value("${oauth.client.secret}")
     private String clientSecret;
@@ -42,7 +42,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient(clientName)
+        clients.inMemory().withClient(clientId)
                 .secret(bCryptPasswordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
